416bbff212bfb1e764847d788c289e91cf2a3739
haykam821 <haykam821@users.noreply.github.com>
2017-09-09 18:28:43 -0400

    Beautified.

--- Onebot.js
+++ Onebot.js
@@ -9,46 +9,48 @@ var plugins = [];
 
 var dir = './plugins';
 
-if (!fs.existsSync(dir)){
-    console.log("Plugins folder don't exist. Creating.");
-    fs.mkdirSync(dir);
+if (!fs.existsSync(dir)) {
+  console.log("Plugins folder don't exist. Creating.");
+  fs.mkdirSync(dir);
 }
 
 var includeAll = require('include-all');
 var controllers = includeAll({
-  dirname           :  path.join(__dirname, 'plugins'),
-  filter            :  /(.+)\.obp$/,
-  flatten           : true,
-  force             : true,
-  keepDirectoryPath : true
+  dirname: path.join(__dirname, 'plugins'),
+  filter: /(.+)\.obp$/,
+  flatten: true,
+  force: true,
+  keepDirectoryPath: true
 });
 
 jsdom.env({
   html: "",
   scripts: ["http://code.jquery.com/jquery.js"],
-  done: function (err, window) {
+  done: function(err, window) {
     $ = window.$;
-    functions = $.map(controllers, function(el) { return el; });
+    functions = $.map(controllers, function(el) {
+      return el;
+    });
   }
 });
 
 var Discord = require('discord.io');
 var bot = new Discord.Client({
-    autorun: true,
-    token: doc.token
+  autorun: true,
+  token: doc.token
 });
 
 function applyVar(thing) {
-    test = eval('thing');
-    test = test.replace(doc.sendervar, sender);
-    test = test.replace(doc.versionvar, version);
-    return test;
+  test = eval('thing');
+  test = test.replace(doc.sendervar, sender);
+  test = test.replace(doc.versionvar, version);
+  return test;
 }
 
 if (doc.startupconsole === "") {
-    console.log("Started up config successfully!");
+  console.log("Started up config successfully!");
 } else {
-    console.log(doc.startupconsole);
+  console.log(doc.startupconsole);
 };
 
 /*bot.on('disconnect', function(event) {
@@ -58,17 +60,17 @@ if (doc.startupconsole === "") {
 });*/
 
 bot.on('ready', function(event) {
-    if (doc.startupevent === "") {
-        bot.sendMessage({
-            to: doc.logchannel,
-            message: "Logged in as <@" + bot.id + ">."
-        });
-    } else {
-        bot.sendMessage({
-            to: doc.logchannel,
-            message: doc.startupconsole
-        });
-    }
+  if (doc.startupevent === "") {
+    bot.sendMessage({
+      to: doc.logchannel,
+      message: "Logged in as <@" + bot.id + ">."
+    });
+  } else {
+    bot.sendMessage({
+      to: doc.logchannel,
+      message: doc.startupconsole
+    });
+  }
 });
 
 var isBot = (doc.usertype == 'auto' && bot.bot) || doc.usertype == 'bot';
@@ -78,11 +80,11 @@ var isBot = (doc.usertype == 'auto' && bot.bot) || doc.usertype == 'bot';
 bot.on('message', function(user, userID, channelID, message, event, messageID) {
   if (!isBot && userID !== bot.id) return;
 
-  try{
-    for(var i = 0; i < functions.length; i++){
+  try {
+    for (var i = 0; i < functions.length; i++) {
       functions[i].onMessageReceived(bot, doc, user, userID, channelID, message, event, messageID);
     }
-  } catch (error){
+  } catch (error) {
     console.log(error);
   }
 });
@@ -91,10 +93,10 @@ var index = 5;
 //texts = yaml.safeLoad(fs.readFileSync('texts.yaml', 'utf8'));
 
 if (doc.reloadconfig !== -1) {
-setInterval(function() {
-doc = yaml.safeLoad(fs.readFileSync('config.yaml', 'utf8'));
-//points = yaml.safeLoad(fs.readFileSync('points.yaml', 'utf8'));
-}, doc.reloadconfig * 1000);
+  setInterval(function() {
+    doc = yaml.safeLoad(fs.readFileSync('config.yaml', 'utf8'));
+    //points = yaml.safeLoad(fs.readFileSync('points.yaml', 'utf8'));
+  }, doc.reloadconfig * 1000);
 };
 
 function randInt(min, max) {
