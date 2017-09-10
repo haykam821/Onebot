93b9341af9009a9449a68eadfadffb5a2513312d
haykam821 <haykam821@users.noreply.github.com>
2017-09-10 10:23:30 -0400

    Beautify

--- exports.js
+++ exports.js
@@ -17,17 +17,17 @@ exports.registerCmd = function(syntax, desc) {
 
   for (i in syntax) {
     if (exports.syntaxes.indexOf(syntax[i]) == -1) {
-       exports.syntaxes.push(syntax[i]);
-       exports.descriptions.push(descgud);
+      exports.syntaxes.push(syntax[i]);
+      exports.descriptions.push(descgud);
     }
   }
 
 }
 
 exports.makeCommand = function(code, syntax, desc) {
-var onebotdesc = desc === undefined ? "No description provided." : desc
-if (exports.syntaxes.indexOf(syntax) == -1 && exports.descriptions.indexOf(desc) == -1) {
-   exports.syntaxes.push(syntax);
+  var onebotdesc = desc === undefined ? "No description provided." : desc
+  if (exports.syntaxes.indexOf(syntax) == -1 && exports.descriptions.indexOf(desc) == -1) {
+    exports.syntaxes.push(syntax);
     exports.descriptions.push(onebotdesc);
-}
+  }
 }
