1b87382bc7d4a57c19accdb3434e2f76a1c9c01a
haykam821 <haykam821@users.noreply.github.com>
2017-09-10 10:22:02 -0400

    Add everything new since last commit
    This is confirmed to work with the Help plugin.

--- exports.js
+++ exports.js
@@ -14,21 +14,24 @@ exports.registerCmd = function(syntax, desc) {
   // Please update to fit this! syntax will be [[command1, command2], opts] but desc stays the same
   // Will remove previous code in a later update, so update quickly.
   var descgud = desc === undefined ? "No description provided." : desc;
-  var command = typeof syntax === "object" ? doc.prefix + syntax[0].join("|" + doc.prefix) : syntax.toString().split(" ")[0];
-  var opts = typeof syntax === "object" ? syntax[1] : syntax.split(" ").slice(1).join(" ");
-  var syntaxopts = syntax[1] === undefined ? "" : syntax[1];
-  var syntaxgud = "<" + command + "> " + opts;
-  console.log(syntaxgud)
-  if (exports.syntaxes.indexOf(syntaxgud) == -1 && exports.descriptions.indexOf(descgud) == -1) {
-    exports.syntaxes.push(syntaxgud);
-    exports.descriptions.push(descgud);
+  //console.log(syntax[0].join('|' + doc.prefix))
+  //var command = typeof syntax === "object" ? doc.prefix + syntax[0].join("|" + doc.prefix) : syntax.toString().split(" ")[0];
+  //var opts = typeof syntax === "object" ? syntax[1] : syntax.split(" ").slice(1).join(" ");
+  //var syntaxopts = syntax[1] === undefined ? "" : syntax[1];
+
+  for (i in syntax) {
+    if (exports.syntaxes.indexOf(syntax[i]) == -1) {
+       exports.syntaxes.push(syntax[i]);
+       exports.descriptions.push(descgud);
+    }
   }
+
 }
 
 exports.makeCommand = function(code, syntax, desc) {
-  var onebotdesc = desc === undefined ? "No description provided." : desc
-  if (exports.syntaxes.indexOf(syntax) == -1 && exports.descriptions.indexOf(desc) == -1) {
-    exports.syntaxes.push(syntax);
+var onebotdesc = desc === undefined ? "No description provided." : desc
+if (exports.syntaxes.indexOf(syntax) == -1 && exports.descriptions.indexOf(desc) == -1) {
+   exports.syntaxes.push(syntax);
     exports.descriptions.push(onebotdesc);
-  }
+}
 }
